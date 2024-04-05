e6a4421099e22b915d0cc9902c4a1e3502fc43f2
helton-quintans <helton.quit@gmail.com>
2024-04-04 21:47:06 -0300

    feat: GroupCard component

--- src/components/Highligth/styles.ts
+++ src/components/Highligth/styles.ts
@@ -1,11 +1,8 @@
-import theme from 'src/theme';
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
     width: 100%;
     margin: 32px 0;
-    /* background-color: ${({ theme }) => theme.COLORS.GRAY_600};
-    padding: 24px; */
 `;
 
 export const Title = styled.Text`
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,19 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highligth } from "@components/Highligth";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
       <Header />
+
       <Highligth 
         title='Classes' 
         subtitle='play with your class'
       />
+
+      <GroupCard title='Galera do COD' />
     </Container>
   );
 }
